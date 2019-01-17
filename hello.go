8885fa9a6942e4ca97e009668e59756dbf9f70ca
John Lovejoy <johnflovejoy@gmail.com>
2019-01-16 21:36:04 -0600

    Created a age doubler function. l33t.

--- hello.go
+++ hello.go
@@ -4,8 +4,14 @@ package main
 
 import "fmt"
 
-	
+
 
 func main() {
-    fmt.Println("hello world")
+		var age int = 32
+    fmt.Printf("hello world, you are %v years old.\n", age)
+		fmt.Printf("You're age doubled is %v years old! \n", doubleYourAge(age))
+}
+
+func doubleYourAge(age int) int{
+	return age * 2
 }
